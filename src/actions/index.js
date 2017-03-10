b4f6269ec752a4155a023583136f67321495f3fe
Wisarut <noomerzx@gmail.com>
2017-03-10 13:00:37 +0700

    remove key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_KEY = '05739810856f7b7644fa251b0ba8fa69'
+const API_KEY = ''
 const API_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
